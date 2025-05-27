15f9497816e84285523622116ee89d798295a025
isafigura <isabela_figura@hotmail.com>
2025-05-27 20:23:23 -0300

    atividade 07

--- atividade07/scripts.js
+++ atividade07/scripts.js
@@ -1,21 +1,24 @@
-const numeroSecreto = Math.random() * 100 + 1;
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let tentativas = 0;
 
+function verificarPalpite() {
+ 
 
-    function verificarPalpite() {
-      const input = document.getElementById("palpite");
-      const palpite = Number(input.value);
+  const input = document.getElementById("palpite");
+  const palpite = Number(input.value);
 
-      if (palpite < 1 || palpite > 100) {
-        alert("Digite um número entre 1 e 100.");
-       
-      }
+  if (palpite < 1 || palpite > 100) {
+    alert("Digite um número entre 1 e 100.");
+    return;
+  }
 
-      if (palpite === numeroSecreto) {
-        document.getElementById("mensagem").innerHTML = 
-          `<strong>Parabéns!</strong> Você acertou o número <strong>${numeroSecreto}</strong> em <strong>${tentativas}</strong> tentativas.`;
-      } else if (palpite < numeroSecreto) {
-        document.getElementById("mensagem").textContent = "O número é maior";
-      } else {
-        document.getElementById("mensagem").textContent = "O número é menor";
-      }
-    }
+  const mensagem = document.getElementById("mensagem");
+
+  if (palpite === numeroSecreto) {
+    mensagem.innerHTML = `<strong>Parabéns!</strong> Você acertou o número <strong>${numeroSecreto}</strong> em <strong>${tentativas}</strong> tentativas.`;
+  } else if (palpite < numeroSecreto) {
+    mensagem.textContent = "O número é maior.";
+  } else {
+    mensagem.textContent = "O número é menor.";
+  }
+}
