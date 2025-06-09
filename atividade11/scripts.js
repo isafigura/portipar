da34cc8bcb7694f732e046613e68f3b19b3f3b67
isafigura <isabela_figura@hotmail.com>
2025-06-09 13:57:19 -0300

    atividades

---
+++ atividade11/scripts.js
@@ -0,0 +1,27 @@
+function calcularNotas() {
+    const valorInput = document.getElementById('valor');
+    const resultadoDiv = document.getElementById('resultado');
+    let valor = parseInt(valorInput.value);
+
+    if (isNaN(valor) || valor < 2) {
+        resultadoDiv.innerHTML = "<p>Digite um valor vÃ¡lido (mÃ­nimo R$2).</p>";
+        return;
+    }
+
+    const notas = [100, 50, 20, 10, 5, 2];
+    let resultado = '';
+
+    notas.forEach(nota => {
+        let qtd = Math.floor(valor / nota);
+        if (qtd > 0) {
+            resultado += `<p>${qtd} nota(s) de R$${nota}</p>`;
+            valor %= nota;
+        }
+    });
+
+    if (valor > 0) {
+        resultado += "<p><strong>NÃ£o Ã© possÃ­vel sacar moedas. Valor restante: R$" + valor + "</strong></p>";
+    }
+
+    resultadoDiv.innerHTML = resultado;
+}
--- scripts.js
+++ scripts.js
@@ -1,44 +1,5 @@
-let timer;          
-let seconds = 0;     
-let isRunning = false; 
+function mudar(tela){
 
+    document.getElementById('tela').src = tela
 
-const timerDisplay = document.getElementById('timer');
-const startBtn = document.getElementById('startBtn');
-const pauseBtn = document.getElementById('pauseBtn');
-const resetBtn = document.getElementById('resetBtn');
-
-function updateDisplay() {
-  const mins = Math.floor(seconds / 60);
-  const secs = seconds % 60;
-  timerDisplay.textContent = 
-    `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
 }
-
-function startTimer() {
-  if (!isRunning) {
-    isRunning = true;
-    timer = setInterval(() => {
-      seconds++;
-      updateDisplay();
-    }, 1000);
-  }
-}
-
-function pauseTimer() {
-  isRunning = false;
-  clearInterval(timer);
-}
-
-function resetTimer() {
-  pauseTimer();
-  seconds = 0;
-  updateDisplay();
-}
-
-startBtn.addEventListener('click', startTimer);
-pauseBtn.addEventListener('click', pauseTimer);
-resetBtn.addEventListener('click', resetTimer);
-
-updateDisplay();
-
